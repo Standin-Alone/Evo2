47f92aeadf8b1dc017496459278eafc5d6cb0bb7
johnedcelzenarosa@gmail.com <johnedcelzenarosa@gmail.com>
2021-08-05 16:55:00 +0800

    August 5, 2021

--- app/Modules/DownloadMobileApplications/routes/web.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-Route::get('download-mobile-applications', 'DownloadMobileApplicationsController@welcome');
--- app/Modules/VoucherTransactions/routes/web.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-Route::get('voucher-transactions', 'VoucherTransactionsController@welcome');
