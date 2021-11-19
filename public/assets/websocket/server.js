59939420a84edce0feb28a4042568627863ab770
Standin-Alone <johnedcelzenarosa@gmail.com>
2021-11-19 15:47:05 +0800

    push

--- public/assets/websocket/server.js
+++ public/assets/websocket/server.js
@@ -1,15 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var redis= require('redis');
-var redis_adapter = require('@socket.io/redis-adapter');
 
-
-
-
-
-var sredis = require('socket.io-redis');
-const { Socket } = require('dgram');
 
 
 io.on('connection', function(socket){
@@ -34,72 +26,6 @@ http.listen(3000, '127.0.0.1', function(data) {
 });
 
 
-
-
-//  pubClient.on('connect', function (err) {
-   
- 
-  
-
-//   pubClient.subscribe('channel');
-   
-//    pubClient.on("message", function (channel, my_channel) {
-
-    
-        
-
- 
-
-
-//   pubClient.psubscribe('messages:'+my_channel,function(message_channel){
-//     pubClient.on("pmessage", function (pattern,channel, message) {
-
-
-       
-//         //Send this event to everyone in the room.
-//         if(message == '100%'){
-          
-
-            
-    
-
-//         }
-        
-//         io.emit('client_room', my_channel);
-        
-//         if(JSON.parse(message).channel == my_channel){
-//           console.warn(my_channel);
-//           io.emit('progress:'+my_channel, {channel : JSON.parse(message).channel , message: JSON.parse(message).percentage});
-//         }
-        
-        
-//      })
-
-      
-      
-      
-//     });
-    
-//   });
-
-
-   
-    
-//  }).on('error', function (error) {
-//   console.log(error);
-// });
-// ;
-
-
-// io.on('connection', function(socket) {
-//   var pubClient = redis.createClient({host:'127.0.0.1,',port:'6379'});
-//   pubClient.subscribe('messages');  
-//   pubClient.on('messages',function(channel, message){
-//     console.log("mew message in queue "+ message + "channel");
-//     console.warn(message);
-//   })
-
-// });
 
 
 
